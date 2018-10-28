73a29617e7ab8ed5ed2ba33668188c3b14e1de6e
yovany.lg <yovany.lg@gmail.com>
2018-10-27 20:36:54 -0500

    Ping to Bots implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var mosca = require('mosca');
 const botClient = require('./client/bot-client');
+const tabletClient = require('./client/tablet-client');
 const { addConnected, removeConnected } = require('./connection-manager');
 const redis = require('redis');
 
@@ -29,8 +30,12 @@ var server = new mosca.Server(moscaSettings);
 server.on('ready', setup);
 
 server.on('clientConnected', function(client) {
-  console.log('client connected', client.id);
-  addConnected(client.id);
+  const { id, connection: { stream: { remoteAddress } } } = client;
+  const ipRegex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g;
+  // console.log(remoteAddress);
+  const ip = remoteAddress ? remoteAddress.match(ipRegex) : undefined;
+  console.log('client connected', { id, ip: ip ? ip[0] : undefined });
+  addConnected(id, ip ? ip[0] : undefined);
 });
 
 server.on('clientDisconnected', function(client) {
@@ -40,15 +45,12 @@ server.on('clientDisconnected', function(client) {
 
 // fired when a message is received
 server.on('published', function(packet, client) {
-  const {
-    payload: { x, y },
-  } = packet;
-
   console.log('Published', packet.topic, packet.payload.toString());
 });
 
 // fired when the mqtt server is ready
 function setup() {
   console.log('Mosca server is up and running');
-  botClient();
+  // botClient();
+  // tabletClient();
 }
